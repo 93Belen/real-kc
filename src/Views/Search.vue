cf4df006cea1215a7d938216718f3d22456db8ac
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-05 17:40:57 -0600

    working on the map

--- src/Views/Search.vue
+++ src/Views/Search.vue
@@ -1,23 +1,19 @@
 <script setup>
-import { router } from '../router'
-import {RouterLink} from 'vue-router'
 import Map from '../components/Map.vue'
-
-const goToHome = () => {
-    router.push('/')
-}
+import GoBack from '../components/GoBack.vue'
+import Add from '../components/Add.vue'
 
 </script>
 
 <template>
 <div class="h-[100dvh] p-6 overflow-hidden box-border">
-    <div class="absolute top-0 left-0 z-[1] h-full p-6 overflow-hidden">
-    <h1 class="text-white">Search</h1>
-    <button @click="goToHome">Go back home</button>
-</div>
-<div class="absolute top-0 left-0 w-full h-[100dvh] z-0 overflow-hidden">
-    <Map/>
-</div>
+    <div class="absolute top-0 left-0 z-[1] h-fit p-6 flex flex-col gap-6">
+        <GoBack/>
+        <Add/>
+    </div>
+    <div class="absolute top-0 left-0 w-full h-[100dvh] z-0 overflow-hidden">
+        <Map/>
+    </div>
 </div>
 </template>
 
---
+++ src/components/Add.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-[42px] h-[42px]">
+        <svg width="100%" height="100%" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M26.0002 17.333V34.6663M17.3335 25.9997H34.6668M47.6668 25.9997C47.6668 37.9658 37.9663 47.6663 26.0002 47.6663C14.034 47.6663 4.3335 37.9658 4.3335 25.9997C4.3335 14.0335 14.034 4.33301 26.0002 4.33301C37.9663 4.33301 47.6668 14.0335 47.6668 25.9997Z" stroke="url(#paint0_linear_40_18304)" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+        <defs>
+        <linearGradient id="paint0_linear_40_18304" x1="4.3335" y1="10.1251" x2="49.6462" y2="45.102" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#42B8A2"/>
+        <stop offset="1" stop-color="#DC44D9"/>
+        </linearGradient>
+        </defs>
+        </svg>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
