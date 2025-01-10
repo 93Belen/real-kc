2e246ebf73df7e800f2e8cf3468e4380e9f77a59
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-09 19:32:09 -0600

    home page

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,20 +1,41 @@
 <script setup>
 import GoToSearch from '../components/GoToSearch.vue'
+import Pin from '../components/Pin.vue'
 
 </script>
 
 <template>
-<div class="w-full min-h-screen bg-base bg-kc-map bg-center bg-contain p-4 flex flex-col gap-4">
+<div class="w-full h-fit min-h-screen bg-base bg-kc-map bg-center bg-contain p-4 flex flex-col gap-4">
     <div class="w-full flex justify-end">
         <GoToSearch/>
     </div>
-    <div class="w-full h-[70vh] flex justify-center items-center">
+    <div class="w-full h-[90dvh] flex justify-center items-center">
         <div class="w-[97%] min-h-fit max-h-[300px] md:max-h-[600px] h-[40dvh] md:p-0 md:w-[600px] md:h-[400px] bg-layer2/60 rounded-xl backdrop-blur-sm text-center flex flex-col justify-center items-center">
             <div class="bg-gradient-to-r from-secondary-red to-secondary-yellow font-black w-fit bg-clip-text text-transparent text-[50px] md:text-[96px] font-h1">
                 <p class="">Search</p>
                 <h1 class="mt-[-30px] md:mt-[-65px]">Real KC</h1>
             </div>
             <button class="italic text-[20px] md:text-[32px] font-body font-bold bg-gradient-to-r from-primary-green to-primary-pink w-[70%] md:w-[50%] flex justify-stretch p-2 rounded-full"><span class="shadow-inner shadow-sm w-full h-full rounded-full">Search</span></button>
+        </div>
+    </div>
+    <div class="w-full h-fit flex md:flex-row gap-12 justify-center items-center flex-col">
+        <div class="w-[90%] h-fit md:w-full flex gap-2 flex-col justify-center items-center">
+            <div><Pin/></div>
+            <div class="bg-gradient-to-br from-primary-green/60 to-primary-pink/60 w-full h-[300px] rounded-xl backdrop-blur-sm">
+
+            </div>
+        </div>
+        <div class="w-[90%] h-fit md:w-full flex gap-2 flex-col justify-center items-center">
+            <div><Pin/></div>
+            <div class="bg-gradient-to-br from-primary-green/60 to-primary-pink/60 w-full h-[300px] rounded-xl backdrop-blur-sm">
+
+            </div>
+        </div>
+        <div class="w-[90%] h-fit md:w-full flex gap-2 flex-col justify-center items-center">
+            <div><Pin/></div>
+            <div class="bg-gradient-to-br from-primary-green/60 to-primary-pink/60 w-full h-[300px] rounded-xl backdrop-blur-sm">
+
+            </div>
         </div>
     </div>
 </div>
---
+++ src/components/Pin.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-[50px] h-[50px]">
+        <svg width="100%" height="100%" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M61.25 29.167C61.25 49.5837 35 67.0837 35 67.0837C35 67.0837 8.75 49.5837 8.75 29.167C8.75 22.2051 11.5156 15.5283 16.4384 10.6054C21.3613 5.68261 28.0381 2.91699 35 2.91699C41.9619 2.91699 48.6387 5.68261 53.5616 10.6054C58.4844 15.5283 61.25 22.2051 61.25 29.167Z" fill="url(#paint0_linear_40_17657)"/>
+        <path d="M35 37.917C39.8325 37.917 43.75 33.9995 43.75 29.167C43.75 24.3345 39.8325 20.417 35 20.417C30.1675 20.417 26.25 24.3345 26.25 29.167C26.25 33.9995 30.1675 37.917 35 37.917Z" fill="url(#paint1_linear_40_17657)"/>
+        <path d="M61.25 29.167C61.25 49.5837 35 67.0837 35 67.0837C35 67.0837 8.75 49.5837 8.75 29.167C8.75 22.2051 11.5156 15.5283 16.4384 10.6054C21.3613 5.68261 28.0381 2.91699 35 2.91699C41.9619 2.91699 48.6387 5.68261 53.5616 10.6054C58.4844 15.5283 61.25 22.2051 61.25 29.167Z" stroke="url(#paint2_linear_40_17657)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M35 37.917C39.8325 37.917 43.75 33.9995 43.75 29.167C43.75 24.3345 39.8325 20.417 35 20.417C30.1675 20.417 26.25 24.3345 26.25 29.167C26.25 33.9995 30.1675 37.917 35 37.917Z" stroke="url(#paint3_linear_40_17657)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <defs>
+        <linearGradient id="paint0_linear_40_17657" x1="8.75" y1="8.95247" x2="69.0817" y2="47.1779" gradientUnits="userSpaceOnUse">
+        <stop offset="0.015" stop-color="#D33B3E"/>
+        <stop offset="1" stop-color="#FFD700"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_40_17657" x1="8.75" y1="8.95247" x2="69.0817" y2="47.1779" gradientUnits="userSpaceOnUse">
+        <stop offset="0.015" stop-color="#D33B3E"/>
+        <stop offset="1" stop-color="#FFD700"/>
+        </linearGradient>
+        <linearGradient id="paint2_linear_40_17657" x1="8.75" y1="8.95247" x2="69.0817" y2="47.1779" gradientUnits="userSpaceOnUse">
+        <stop offset="0.015" stop-color="#D33B3E"/>
+        <stop offset="1" stop-color="#FFD700"/>
+        </linearGradient>
+        <linearGradient id="paint3_linear_40_17657" x1="8.75" y1="8.95247" x2="69.0817" y2="47.1779" gradientUnits="userSpaceOnUse">
+        <stop offset="0.015" stop-color="#D33B3E"/>
+        <stop offset="1" stop-color="#FFD700"/>
+        </linearGradient>
+        </defs>
+        </svg>
+    </div>
+</template>
