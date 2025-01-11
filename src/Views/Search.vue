fa1b9565724b35489956462ab56febc088311fe1
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-11 16:15:11 -0600

    draggable

--- src/Views/Search.vue
+++ src/Views/Search.vue
@@ -4,6 +4,26 @@ import GoBack from '../components/GoBack.vue'
 import Add from '../components/Add.vue'
 import Filters from '../components/Filters.vue'
 import CardFeed from '../components/CardFeed.vue'
+import { gsap } from "gsap"; 
+import { Draggable } from "gsap/Draggable";
+import { onMounted } from 'vue'
+
+gsap.registerPlugin(Draggable);
+
+onMounted(() => {
+    const mobile = window.innerWidth < 768
+    if(mobile){
+            Draggable.create(".draggable", {
+            type: "y",
+            handle: '.handle',
+            bounds: {
+                minY: 0,
+                maxY: 150
+            },
+            dragResistance: 0.1,
+        });
+    }
+})
 
 </script>
 
@@ -14,10 +34,10 @@ import CardFeed from '../components/CardFeed.vue'
         <GoBack/>
         <Add/>
     </div>
-    <div class="absolute bottom-[190px] md:bottom-[30px] left-0 z-[1] w-[99vw] overflow-x-hidden">
+    <div class="absolute draggable handle bottom-[190px] md:bottom-[30px] left-0 z-[1] w-[99vw] overflow-x-hidden">
         <Filters/>
     </div>
-    <div class="absolute ml-[-21px] md:ml-0 bottom-[10px] md:top-0 md:right-[15px] w-[99vw] md:w-fit md:h-[99dvh] z-[2] overflow-y-hidden md:overflow-y-auto md:overflow-x-hidden">
+    <div class="absolute draggable handle ml-[-21px] md:ml-0 bottom-[10px] md:top-0 md:right-[15px] w-[99vw] md:w-fit md:h-[99dvh] z-[2] overflow-y-hidden md:overflow-y-auto md:overflow-x-hidden">
         <CardFeed/>
     </div>
     <!-- Map -->
