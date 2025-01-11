363cd33f7f150503ba78fb9998b9a765a974382c
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-11 15:55:56 -0600

    fixing

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -6,7 +6,7 @@ const filters = filterStore.filters
 </script>
 
 <template>
-    <div class="flex gap-6 min-w-full overflow-x-scroll">
+    <div class="flex gap-6 min-w-full pr-[20px] md:pr-[400px] overflow-x-scroll">
         <div class="w-[20px] md:w-[50px]"></div>
         <FilterChip @click="filterStore.select(filter.name)" v-for="(filter, index) in filters" v-bind:key="index" :filter="filter" />
     </div>
