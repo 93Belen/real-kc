66bcadaf8db62e656a8c7b55a8d0db08e2ee275c
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-11 20:20:01 -0600

    card content

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,18 @@
+<script setup>
+import Edit from './Edit.vue'
+import Trash from './Trash.vue'
+
+</script>
+
 <template>
-    <div class="w-[300px] md:w-[350px] h-[170px] bg-layer2 rounded-xl">
-        
+    <div class="w-[300px] md:w-[350px] h-[170px] flex flex-col justify-around p-4 bg-layer2 rounded-xl font-body text-white text-[14px]">
+        <h2 class="font-bold">Baba's Pantry</h2>
+        <a href="">6913 W 123rd Terrace Apt 714, 6609, Overland Park</a>
+        <p>Palestinian restaurant. Falafel, Shawarman... Try the french fries!</p>
+        <div class="w-full flex justify-between gap-4">
+            <p>Restaurant <span>|</span> Bakery</p>
+            <Edit/>
+            <Trash/>
+        </div>
     </div>
 </template>
