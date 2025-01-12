3b3323c41ae17ab50a982b373534d327a8e06769
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-11 20:37:28 -0600

    card content

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,12 +5,12 @@ import Trash from './Trash.vue'
 </script>
 
 <template>
-    <div class="w-[300px] h-[170px] flex flex-col justify-around p-4 bg-layer2 rounded-xl font-body text-white text-[14px]">
+    <div class="w-[300px] h-[170px] font-extralight flex flex-col justify-around p-4 bg-layer2 rounded-xl font-body text-white text-[14px]">
         <h2 class="font-medium">Baba's Pantry</h2>
-        <a class="underline decoration-primary-pink font-thin" href="">6913 W 123rd Terrace Apt 714, 6609, Overland Park</a>
-        <p class="font-thin">Palestinian restaurant. Falafel, Shawarman... Try the french fries!</p>
+        <a class="underline decoration-primary-pink" href="">6913 W 123rd Terrace Apt 714, 6609, Overland Park</a>
+        <p class="">Palestinian restaurant. Falafel, Shawarman... Try the french fries!</p>
         <div class="w-full flex justify-between gap-4">
-            <p class="font-thin">Restaurant <span class="text-primary-green">|</span> Bakery</p>
+            <p class="">Restaurant <span class="text-primary-green">|</span> Bakery</p>
             <div class="w-fit gap-2 flex">
                 <Edit/>
                 <Trash/>
