c380ddf6b2441f8ae4c4ff577d3a99de71f422d8
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-12 16:05:00 -0600

    touch

--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -75,9 +75,9 @@ const validateAndSave = () => {
 
 <template>
     <div class="h-[87dvh] w-screen font-body md:px-12 bg-layer3 rounded-t-xl md:rounded-b-xl md:w-[60vw] md:h-[80vh] md:m-auto md:bg-layer3/60 backdrop-blur-sm flex flex-col justify-around p-4">
-        <input :class="missingName ? 'border-red' : 'border-white'" required v-model="content.name" class="border-b-2 border-white bg-transparent outline-none" maxlength="20" type="text" placeholder="Business name">
-        <textarea :class="missingDescription ? 'border-red' : 'border-white'"  required v-model="content.description" class="border-b-2 border-white bg-transparent outline-none" maxlength="60" placeholder="description" name="" id="" cols="30" rows="1"></textarea>
-        <input :class="missingAddress ? 'border-red' : 'border-white'"  required v-model="content.address" class="border-b-2 border-white bg-transparent outline-none" type="text" placeholder="Address">
+        <input :class="missingName ? 'border-red' : 'border-white'" required v-model="content.name" class="border-b-2 bg-transparent outline-none" maxlength="20" type="text" placeholder="Business name">
+        <textarea :class="missingDescription ? 'border-red' : 'border-white'"  required v-model="content.description" class="border-b-2 bg-transparent outline-none" maxlength="60" placeholder="description" name="" id="" cols="30" rows="1"></textarea>
+        <input :class="missingAddress ? 'border-red' : 'border-white'"  required v-model="content.address" class="border-b-2 bg-transparent outline-none" type="text" placeholder="Address">
         <div class="flex flex-col gap-2">
             <label :class="missingBusinessType ? 'text-red' : ''" class="text-white" for="type">Select max 2 business type</label>
             <div id="type" class="flex gap-6 min-w-full pr-[20px] overflow-x-scroll">
