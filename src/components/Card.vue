6a446a713db44cf602cd5604c78eb8da821145b8
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-12 06:39:31 -0600

    db

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,12 +2,12 @@
 import Edit from './Edit.vue'
 import Trash from './Trash.vue'
 const props = defineProps(['data'])
-let { name, description, address, type } = props.data
+let { name, description, address, type, id } = props.data
 
 </script>
 
 <template>
-    <div class="w-[300px] h-[170px] font-extralight flex flex-col justify-around p-4 bg-layer2 rounded-xl font-body text-white text-[14px]">
+    <div :id="id" class="w-[300px] h-[170px] font-extralight flex flex-col justify-around p-4 bg-layer2 rounded-xl font-body text-white text-[14px]">
         <h2 class="font-medium">{{name}}</h2>
        <a target="_blank" class="underline decoration-primary-pink" :href="'https://www.google.com/maps/search/?api=1&query=' + encodeURIComponent(address)">{{ address }}</a>
         <p class="">{{description}}</p>
