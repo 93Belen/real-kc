016a9c4120d37fdbcf2f14edc2d4d7a8ebcb10e1
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-18 13:23:05 -0600

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,21 @@
 <script setup>
 import Hero from './Hero.vue'
+import Heart from './Heart.vue'
 </script>
 
 
 <template>
     <div class="w-full h-full flex flex-col md:flex-row justify-between p-6 text-lg md:items-end">
-        <div class="flex flex-col gap-6">
-            <p>Report a bug</p>
+        <div class="flex flex-col gap-6 md:w-[30vw]">
             <p>Donate</p>
             <p>Contact</p>
+            <p>Report a bug</p>
         </div>
         <div class="hidden md:block h-full w-auto">
             <Hero :inFooter="true" />
         </div>
-        <div class="self-end">
-            <p>Search Real KC</p>
+        <div class="self-end flex items-end justify-end md:w-[30vw]">
+            <p class="flex gap-2 justify-center items-center text-sm italic font-thin">Made with <span><Heart/></span> by <a target="_blank" href="https://clever-raindrop-40359f.netlify.app/">Belencita</a></p>
         </div>
     </div>
 </template>
