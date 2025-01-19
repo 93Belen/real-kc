d826b09d6ffbb5768d73209005db9d5f1c12312b
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-19 07:36:56 -0600

    tweaking

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,9 @@ import Heart from './Heart.vue'
 import { router } from '../router'
 import {RouterLink} from 'vue-router'
 import { useToast } from 'vue-toastification'
+import Copy from './Copy.vue'
+import Search from './Search.vue'
+
 
 const toast = useToast()
 
@@ -28,8 +31,8 @@ function copyUrl() {
 <template>
     <div class="w-full h-full flex flex-col md:flex-row justify-between p-6 md:p-8 text-lg gap-6 md:gap-0 md:items-end">
         <div class="flex text-md font-thin flex-col gap-4 md:w-[30vw] md:self-end">
-            <button @click="goToSearch" class="w-fit hover:underline underline-offset-1 hover:underline-offset-4 hover:text-primary-green duration-1000 decoration-primary-white hover:decoration-secondary-yellow">Search</button>
-            <button @click="copyUrl" class="cursor-pointer w-fit hover:underline underline-offset-1 hover:underline-offset-4 hover:text-primary-green duration-1000 decoration-primary-white hover:decoration-secondary-yellow">Copy Link</button>
+            <button @click="goToSearch" class="w-fit hover:underline underline-offset-1 hover:underline-offset-4 hover:text-primary-green duration-1000 decoration-primary-white hover:decoration-secondary-yellow flex gap-2 justify-center items-center"><span><Search/></span>Search</button>
+            <button @click="copyUrl" class="cursor-pointer w-fit hover:underline underline-offset-1 hover:underline-offset-4 hover:text-primary-green duration-1000 decoration-primary-white hover:decoration-secondary-yellow flex gap-2 justify-center items-center"><span><Copy/></span> Copy Link</button>
             <!-- <button class="w-fit hover:underline underline-offset-1 hover:underline-offset-4 hover:text-primary-green duration-1000 decoration-primary-white hover:decoration-secondary-yellow">Report a bug</button> -->
         </div>
         <div class="hidden md:block h-full w-auto">
