231211d396aa0c37b5bdc89d336ca5505fcba6ad
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-18 19:28:35 -0600

    pointer in chips

--- src/components/FilterChip.vue
+++ src/components/FilterChip.vue
@@ -4,7 +4,7 @@ let { name } = props.filter
 </script>
 
 <template>
-    <div class="bg-primary-green box-border w-[130px] max-w-[130px] min-w-[130px] py-[5px] h-[35px] text-center rounded-full">
+    <div class="cursor-pointer bg-primary-green box-border w-[130px] max-w-[130px] min-w-[130px] py-[5px] h-[35px] text-center rounded-full">
         <div :class="props.filter.selected ? '':'bg-layer1'" class="boder-2 duration-[1s] m-auto rounded-full w-[90%] h-full flex justify-center items-center">
             <p>{{name}}</p>
         </div>
