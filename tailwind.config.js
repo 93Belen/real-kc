0cf1c9ccd648242a4be0b20eaf5dae6b2262e918
Belen Garcia Martinez <belengm.dev@gmail.com>
2025-01-20 08:00:34 -0600

    flicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,7 @@ export default {
       },
       animation: {
         bounce: 'bounce 1s',
+        flicker: 'flicker 7s infinite'
       },
       keyframes: {
         bounce: {
@@ -62,6 +63,80 @@ export default {
           '80%': {
             transform: 'translateY(-20%)',
             animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+          },
+        },
+        flicker: {
+          '0%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '1',
+          },
+          '10%': {
+            boxShadow: '0px 0px 20px 8px #42B8A2',
+            opacity: '1',
+          },
+          '20%': {
+            boxShadow: '0px 0px 10px 3px #42B8A2',
+            opacity: '0.9',
+          },
+          '30%': {
+            boxShadow: '0px 0px 25px 12px #33A18C',
+            opacity: '1',
+          },
+          '40%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '0.7',
+          },
+          '45%': {
+            boxShadow: '0px 0px 30px 10px #33A18C',
+            opacity: '1',
+          },
+          '50%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '1',
+          },
+          '55%': {
+            boxShadow: '0px 0px 10px 3px #42B8A2',
+            opacity: '0.6',
+          },
+          '56%': {
+            boxShadow: '0px 0px 10px 3px #42B8A2',
+            opacity: '0.8',
+          },
+          '57%': {
+            boxShadow: '0px 0px 10px 3px #42B8A2',
+            opacity: '0.4',
+          },
+          '58%': {
+            boxShadow: '0px 0px 10px 3px #42B8A2',
+            opacity: '0.5',
+          },
+          '59%': {
+            boxShadow: '0px 0px 10px 3px #42B8A2',
+            opacity: '01',
+          },
+          '80%': {
+            boxShadow: '0px 0px 25px 8px #33A18C',
+            opacity: '1',
+          },
+          '90%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '0.8',
+          },
+          '91%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '0.5',
+          },
+          '92%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '0.7',
+          },
+          '93%': {
+            boxShadow: '0px 0px 15px 5px #42B8A2',
+            opacity: '0.2',
+          },
+          '100%': {
+            boxShadow: '0px 0px 20px 6px #42B8A2',
+            opacity: '1',
           },
         },
       }
